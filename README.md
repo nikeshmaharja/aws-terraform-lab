bfae293320c1b4de6e0a4071ee24b859a5ed3e1d
Nikesh Maharjan <nikesh@ssci.co.jp>
2025-02-07 17:00:28 +0900

    Add readm me and comments

---
+++ README.md
@@ -0,0 +1,22 @@
+# AWS Terraform Lab
+
+Terraform configuration to manage a free-tier EC2 instance on AWS.
+
+# Prerequisites
+- Install AWS CLI   (https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html)
+- Install Terraform (https://developer.hashicorp.com/terraform/install?product_intent=terraform)
+- AWS account       (need  AWS Access key ID & Secret access key) Dont use root access (Best Practises)
+
+## Features
+- Creates a t2.micro instance
+- Auto-generates SSH key pair
+- Restricts SSH access to your current IP
+- Cleanup with `terraform destroy`
+
+## Usage
+```bash
+terraform init
+terraform fmt
+terraform apply
+terraform fmt
+terraform destroy
--- main.tf
+++ main.tf
@@ -1,28 +1,40 @@
-resource "aws_key_pair" "lab_key" {
+
+resource "aws_key_pair" "lab_key" {                                 
   key_name   = var.key_name
   public_key = tls_private_key.rsa.public_key_openssh
 }
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
+
+# Generate an SSH key pair using the TLS provider
+resource "tls_private_key" "rsa" {                                  
+  algorithm = "RSA"                                 # Use RSA algorithm for key generation
+  rsa_bits  = 4096                                  # Create a 4096-bit key for enhanced security
 }
 
-resource "aws_instance" "free_tier_instance" {
-  ami           = "ami-0a290015b99140cd1"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.lab_key.key_name
 
+# Create an EC2 instance using the free-tier eligible t2.micro instance type
+resource "aws_instance" "free_tier_instance" {
+  ami           = "ami-0a290015b99140cd1"           # Ubuntu 24 (AMI) ID for the instance 
+  instance_type = "t2.micro"                        # Instance type (free-tier eligible)
+  key_name      = aws_key_pair.lab_key.key_name     # Attach the SSH key pair to the instance
+
+
+# Attach the security group to control inbound/outbound traffic
   security_groups = [aws_security_group.lab_test.name]
 
+
+# Add tags to the instance for better identification
   tags = {
     Name = "Linux-Terra-AWS-TEST"
   }
 }
 
+# Create a security group to control access to the EC2 instance
 resource "aws_security_group" "lab_test" {
-  name        = "lab-security-group"
-  description = "Allow SSH access"
+  name        = "lab-security-group"                # Name of the security group
+  description = "Allow SSH access"                  # Description of the security group's purpose
 
+
+# Ingress rule: Allow SSH access (port 22) only from your current IP address
   ingress {
     from_port   = 22
     to_port     = 22
@@ -30,6 +42,8 @@ resource "aws_security_group" "lab_test" {
     cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
   }
 
+
+# Egress rule: Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
@@ -38,10 +52,13 @@ resource "aws_security_group" "lab_test" {
   }
 }
 
+# Fetch your current public IP address using an HTTP data source
 data "http" "myip" {
   url = "https://ipv4.icanhazip.com"
 }
 
+
+# Save the private key to a local file for SSH access
 resource "local_file" "private_key" {
   content  = tls_private_key.rsa.private_key_pem
   filename = "${var.key_name}.pem"
